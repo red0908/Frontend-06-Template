724ffead9791426ac3e6cfb8d188892b3eb8facf
tong <332673227@qq.com>
2020-12-30 13:54:16 +0800

    wee10:作业

--- Week 10/作业代码/server.js
+++ Week 10/作业代码/server.js
@@ -14,16 +14,23 @@ http.createServer((request, response) => {
 <head>
   <title>Document</title>
   <style>
+    body {
+      background-color: rgb(0,0,0);
+    }
     #container {
       width: 500px;
       height: 300px;
       display: flex;
+      background-color: rgb(255,255,255);
     }
     #container #a {
-      width: 200px
+      width: 200px;
+      height: 100px;
+      background-color: rgb(255,0,0);
     }
     #container #b {
-      flex: 1
+      flex: 1;
+      background-color: rgb(0,255,0);
     }
   </style>
 </head>
